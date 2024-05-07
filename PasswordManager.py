69926817796a7dc264d1544518d6d6d5fa86d866
caydeny <96162241+caydeny@users.noreply.github.com>
2024-05-07 01:59:57 -0700

    Update PasswordManager.py
    added functions

--- PasswordManager.py
+++ PasswordManager.py
@@ -1,11 +1,19 @@
 import random
 import string
 
+master_password = "12345"
+
 def generate_random_password():
     length = random.randint(8, 100) 
     characters = string.ascii_letters + string.digits + string.punctuation
     password = ''.join(random.choice(characters) for _ in range(length))
     return password
+
+def check_password(user_password):
+    if user_password == master_password:
+        return True
+    else:
+        return False
 
 print("PASSWORD MANAGER")
 
@@ -19,10 +27,24 @@ while loop:
     # user input one of follwing options
     option = input()
     if option == "1":
-        password = generate_random_password()
-        print(password)
+        generated_password = generate_random_password()
+        print(generated_password)
     elif option == "2":
-        print("2")
+        print("Please enter the master password: ")
+        
+        attempt = 0
+        while attempt < 3:
+            user_password = input()
+            if check_password(user_password) == True:
+                print("Password matches.")
+                break
+            else:
+                print("Password incorrect. x attempts remaining.")
+                attempt += 1
+                if attempt == 3:
+                    print("Maximum attempts exceeded. Terminating program.")
+                    exit()
+        
     elif option == "3":
         loop = False
     else:
