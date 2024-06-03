d86b6ee98cf061f139b64446fe9e43c98477215d
caydeny <caydenyoo@hotmail.com>
2024-06-02 22:24:27 -0700

    Update Tkinter.py

--- Tkinter.py
+++ Tkinter.py
@@ -1,24 +1,27 @@
 import tkinter as tk
 
-window = tk.Tk()
+window = tk.Tk() # root of application
+window.title("Key Guard")
+window.geometry("750x400")
+window.configure(bg='#333333')
 
-label = tk.Label(
-    text="Python rocks!",
-    fg="white", # textcolour
-    bg="black", # background colour
-    width=10,
-    height=10
-) # tk.Label to add text to window
+# Creating widgets
+login_label = tk.Label(window, text="Login")
 
-button = tk.Button(
-    text="Click me",
-    width=25,
-    height=5,
-    bg="blue",
-    fg="yellow"
-)
+username_label = tk.Label(window, text="Username")
+username_entry = tk.Entry(window)
 
-label.pack() # .pack to add to window
-button.pack()
+password_label = tk.Label(window, text="Password")
+password_entry = tk.Entry(window, show="*")
+
+login_button = tk.Button(window, text="Login")
+
+# Placing widgets
+login_label.grid(row=0, column=0, columnspan=2)
+username_label.grid(row=1, column=0)
+username_entry.grid(row=1, column=1)
+password_label.grid(row=2, column=0)
+password_entry.grid(row=2, column=1)
+login_button.grid(row=3, column=0, columnspan=2)
 
 window.mainloop()
